1c5c4aa4bde61f4b66916984a334e8568c7ba05b
Vladislav Supalov <vladislav@supalov.com>
2015-05-20 16:30:34 +0200

    HandRank lives here now.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,40 @@ extern crate cards;
 
 use cards::card::{Card};
 
-//TODO: this could be an option as well
-//pub type CardSlot = Option<Card>;
+// TODO: impl ORDering?
+#[derive(Debug)]
+#[derive(PartialEq)]
+#[derive(Eq, Hash)]
+pub enum HandRankClass {
+    HighCard,
+    OnePair,
+    TwoPair,
+    ThreeOfAKind,
+    Straight,
+    Flush,
+    FullHouse,
+    FourOfAKind,
+    StraightFlush,
+}
+
+pub type HandRank = u32; // smaller values are "better", Cactus Kev convention
+
+//TODO: functions to compare hand ranks -> find best?
+
+pub fn hand_rank_to_class(val: &HandRank) -> HandRankClass {
+    match *val {
+        x if x > 6185 => HandRankClass::HighCard,
+        x if x > 3325 => HandRankClass::OnePair,
+        x if x > 2467 => HandRankClass::TwoPair,
+        x if x > 1609 => HandRankClass::ThreeOfAKind,
+        x if x > 1599 => HandRankClass::Straight,
+        x if x > 322  => HandRankClass::Flush,
+        x if x > 166  => HandRankClass::FullHouse,
+        x if x > 10   => HandRankClass::FourOfAKind,
+        x if x > 0    => HandRankClass::StraightFlush,
+        _             => panic!("Unexpected hand rank value!")
+    }
+}
 
 pub enum CardSlot {
     Empty,
@@ -18,22 +50,6 @@ impl CardSlot {
         }
     }
 }
-
-#[derive(Debug)]
-#[derive(PartialEq)] 
-#[derive(Eq, Hash)]
-pub enum HandRankClass {
-    HighCard,
-    OnePair,
-    TwoPair,
-    ThreeOfAKind,
-    Straight,
-    Flush,
-    FullHouse,
-    FourOfAKind,
-    StraightFlush,
-}
-
 
 pub struct HandCards(pub CardSlot, pub CardSlot);
 pub struct CommunityCards(pub CardSlot, pub CardSlot, pub CardSlot, pub CardSlot, pub CardSlot);

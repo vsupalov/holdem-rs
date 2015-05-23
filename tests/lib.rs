71ef40b990dc93216eef36cab9dad74c4c4303a5
Vladislav Supalov <vladislav@supalov.com>
2015-05-24 01:02:44 +0200

    Adjusted tests.

--- tests/lib.rs
+++ tests/lib.rs
@@ -26,10 +26,16 @@ fn ordering_of_handrankclass() {
 
 #[test]
 fn hand_rank_to_class_conversion() {
-   assert_eq!(hand_rank_to_class(&0), HandRankClass::HighCard);
+   assert_eq!(hand_rank_to_class(&0), HandRankClass::HighCard); // the worst hand rank
    assert_eq!(hand_rank_to_class(&1276), HandRankClass::HighCard);
    assert_eq!(hand_rank_to_class(&1277), HandRankClass::OnePair);
    assert_eq!(hand_rank_to_class(&(1277+2860)), HandRankClass::TwoPair);
    assert_eq!(hand_rank_to_class(&7451), HandRankClass::FourOfAKind);
-   assert_eq!(hand_rank_to_class(&7461), HandRankClass::StraightFlush);
+   assert_eq!(hand_rank_to_class(&7461), HandRankClass::StraightFlush); // the best hand rank
+}
+
+#[test]
+#[should_panic]
+fn too_large_hand_rank() {
+    hand_rank_to_class(&7462);
 }
